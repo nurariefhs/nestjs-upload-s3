d1c7b519ae0cd858ea240ea6cd35d103a0085db8
Nur Arief <nurariefhs@gmail.com>
2021-11-27 04:25:26 +0700

    [ADD] Basic single upload route

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
+import { Express } from "express";
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+
+  @Post('single')
+  @UseInterceptors(FileInterceptor('file'))
+  async single(
+    @UploadedFile() file: Express.Multer.File
+  ){
+    console.log(file)
+  }
 }
